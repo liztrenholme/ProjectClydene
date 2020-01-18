679e135687580c43110d6224c0737fb2647a2a58
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-01-17 23:40:58 -0500

    list and modal placeholders

---
+++ src/components/List/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import css from './list.css'
+
+export default class List extends Component {
+  static propTypes = {
+    text: PropTypes.string,
+    theme: PropTypes.string
+  }
+
+  render() {
+    const {
+      placeholder, theme
+    } = this.props
+
+    return (
+      <div className={theme ? css.theme : css.listContainer}>
+        <input placeholder={placeholder} />
+      </div>
+    )
+  }
+}
